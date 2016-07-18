db798c9c6b755d068371a52b3c426a7edc38155a
Olivia Taylor <obt1@pitt.edu>
2016-07-18 08:40:30 -0400

    first commit

---
+++ FlyTri/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+
+        Vector3 force = new Vector3(moveHorizontal, 0, 0);
+
+        rb.AddForce(force * speed);
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Pick Up"))
+    //    {
+    //        other.gameObject.SetActive(false);
+    //    }
+    }
